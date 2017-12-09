0aa34b3e96276f9bd2a73cc0cd5498c0435d483d
JacekGdansk87 <jacek515166803@wp.pl>
2017-12-09 14:30:21 +0100

    0.3
    Stage 0.3
    Added keys  to rotate : 'q and 'w'.
    Added new shape of  blocks.
    Repaired bug: collision with the floor.

--- tetris.js
+++ tetris.js
@@ -12,17 +12,15 @@ window.onload = function(){
 const cw = canv.width;
 const ch = canv.height;
 
-const matrix = [
-        [0,0,0],
-        [1,1,1],
-        [0,1,0],
-];
 
-function collide(arena, player){
-    const [m, o] = [player.matrix, player.pos];
-    for(let y = 0; y < m.length; ++y){
-        for(let x = 0; x < m[y].length; ++x){
-            if(m[y][x] !==0 && (arena[y+o.y] && arena[y + o.y][x + o.x]) !==0){
+function collide(arena, player) {
+    const m = player.matrix;
+    const o = player.pos;
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                 arena[y + o.y][x + o.x]) !== 0) {
                 return true;
             }
         }
@@ -38,6 +36,38 @@ function createMatrix(w, h){
     return matrix;
 }
 
+function createBlock(type){
+    if(type === 'T'){
+        return[
+            [0,0,0],
+            [1,1,1],
+            [0,1,0],
+    ];
+}else if(type === 'X'){
+        return[
+            [0,1,0],
+            [1,1,1],
+            [0,1,0],
+    ];
+}else if(type === 'O'){
+        return[
+            [1,1,1],
+            [1,0,1],
+            [1,1,1],
+    ];
+}else if(type === 'I'){
+        return[
+            [0,1,0],
+            [0,1,0],
+            [0,1,0],
+    ];
+}else if(type === 'K'){
+        return[
+            [1,1],
+            [1,1],
+    ];
+}
+}
 
 function drawMatrix(matrix, offset) {
     matrix.forEach((row,y) =>{
@@ -50,19 +80,12 @@ function drawMatrix(matrix, offset) {
     });
 }
 
-const arena = createMatrix(12, 20);
-
-const player = {
-    pos: {x:5, y:5},
-    matrix: matrix,
-}
-
 function playerDrop(){
     player.pos.y++;
     if(collide(arena, player)){
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;  
+        playerReset();
     }
 }
 
@@ -86,6 +109,50 @@ function merge(arena, player){
     });
 }
 
+function playerReset(){
+    const blocks = 'TXOIK';
+    player.matrix = createBlock(blocks[blocks.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0)-
+                    (player.matrix[0].length / 2 | 0);
+    
+}
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if(offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+        
+    }
+}
+
+function rotate(matrix, dir){
+    for(let y = 0; y < matrix.length; ++y){
+        for(let x = 0; x < y; ++x){
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ]=[
+                matrix[y][x],
+                matrix[x][y], 
+                ];
+        }
+    }
+    
+    if (dir > 0){
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
 
 document.addEventListener("keydown", ev =>{
     if(ev.keyCode === 37) {
@@ -99,8 +166,18 @@ document.addEventListener("keydown", ev =>{
             player.pos.x--
             }
     }else if(ev.keyCode === 40) {
-            player.pos.y++;
             playerDrop();
+        //rotate on 'q' and w
+    }else if(ev.keyCode === 81) {
+            playerRotate(-1);
+    }else if(ev.keyCode === 87) {
+            playerRotate(1);
     }
 });
 
+const arena = createMatrix(12, 20);
+
+const player = {
+    pos: {x: 0, y: 0},
+    matrix : createBlock('T'),
+};
